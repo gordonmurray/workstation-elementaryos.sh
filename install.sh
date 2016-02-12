7ed8a17151ab47906c7c7c39206699b80421032d
gordonmurray <gordon@gordonmurray.com>
2016-02-12 14:02:58 +0000

    Install Git and Dropbox

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@ sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /et
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install google-chrome-stable
+
+# Install Git
+ sudo apt-get install -y git
+
+# Install Dropbox
+git clone https://github.com/zant95/elementary-dropbox /tmp/elementary-dropbox
+bash /tmp/elementary-dropbox/install.sh
