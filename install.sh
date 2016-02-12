66d3880e0390e9204a066cf79de1e7c8fa67016a
Gordon <gordon@gordonmurray.com>
2016-02-12 16:14:35 +0000

    add composer steps

--- install.sh
+++ install.sh
@@ -16,3 +16,10 @@ sudo apt-get install remmina
 
 # Install Gparted (for disk management stuff)
 sudo apt-get install gparted
+
+# Install Composer globally
+php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '781c98992e23d4a5ce559daf0170f8a9b3b91331ddc4a3fa9f7d42b6d981513cdc1411730112495fbf9d59cffbf20fb2') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); }"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
